be56e3902676363ec41cbe6861548c5a22f2dd79
Souris.CLOUD <me@souris.cloud>
2021-03-14 18:10:53 +0100

    npcs: despawn (maybe found error in integer from argument)

--- src/main/java/cloud/souris/aytos/mc/safari/SafariCommands.java
+++ src/main/java/cloud/souris/aytos/mc/safari/SafariCommands.java
@@ -67,8 +67,8 @@ public class SafariCommands {
                     break;
                 case "despawn":
                     try {
+                        instance.getLogger().info("convert args[1] to integer");
                         int npcId = Integer.getInteger(args[1]);
-                        instance.getLogger().info("argument int: " + npcId);
                         Entity entity = sender.getLevel().getEntity(npcId);
                         instance.getLogger().info("checking null");
                         if (entity != null) {
